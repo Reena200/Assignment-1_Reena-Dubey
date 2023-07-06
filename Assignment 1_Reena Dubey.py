21cda2f8c9065dc352078c31b2a65bcc7e9109b2
Reena200 <138387440+Reena200@users.noreply.github.com>
2023-07-06 23:58:16 +0530

    Add files via upload

---
+++ Assignment 1_Reena Dubey.py
@@ -0,0 +1,9 @@
+x=int(input("Enter the number1:"))
+y=int(input("Enter the number2:"))
+z=int(input("Enter the number3:"))
+
+print("Given number are:",x,y,z)
+
+avg=(x+y+z)/3
+
+print("average of given number are:",avg)
